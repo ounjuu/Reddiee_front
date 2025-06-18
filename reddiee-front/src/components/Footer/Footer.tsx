316433dbb5e1c7b55632fa4c98a0ca3a13592d43
ounjuu <koj1555@naver.com>
2025-06-18 17:12:04 +0900

    Feat: 푸터

---
+++ reddiee-front/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+      <div className="Footer_eventFlow">이벤트 창</div>
+      <h1>푸터입니다</h1>
+    </div>
+  );
+}
