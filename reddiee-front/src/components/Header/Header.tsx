a7ef3f2f7d034c4bbf4a5cb6631b14060607427f
ounjuu <koj1555@naver.com>
2025-06-23 18:45:57 +0900

    Feat: google font

--- reddiee-front/src/components/Header/Header.tsx
+++ reddiee-front/src/components/Header/Header.tsx
@@ -1,5 +1,12 @@
 "use client";
 import { useState } from "react";
+import { Titan_One } from "next/font/google";
+
+const titanOne = Titan_One({
+  subsets: ["latin"], // 라틴 문자 지원
+  weight: "400", // Titan One은 보통 400(regular)만 있음
+  variable: "--font-titan-one",
+});
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,10 +17,10 @@ export default function Header() {
       <div className="Header_eventFlow"></div>
 
       {/* 메뉴바 */}
-      <div className="Header_nav flex justify-between items-center px-4 py-2 text-reddieetext">
+      <div className="Header_nav flex justify-between items-center px-5 py-6 text-reddieetext backdrop-blur">
         {/* 왼쪽 - 햄버거 버튼 */}
         <button
-          className="flex flex-col justify-between w-6 h-5"
+          className="flex flex-col justify-between w-6 h-5 cursor-pointer"
           onClick={() => setIsOpen((prev) => !prev)}
         >
           <span
@@ -34,12 +41,15 @@ export default function Header() {
         </button>
 
         {/* 가운데 로고 */}
-        <div className="text-lg font-bold absolute left-1/2 -translate-x-1/2">
-          REDDIEE
+        <div
+          className={`text-2xl absolute left-1/2 -translate-x-1/2 cursor-pointer ${titanOne.variable} tracking-wide`}
+          style={{ fontFamily: "var(--font-titan-one)" }}
+        >
+          Reddie
         </div>
 
         {/* 오른쪽 메뉴 */}
-        <div className="flex gap-3 text-sm">
+        <div className="flex gap-3 text-sm cursor-pointer">
           <div>Login</div>
           <div>Cart</div>
           <div>My Page</div>
