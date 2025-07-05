39ed5c8012781f2d51989ee8a89994f4d470cc5c
ounjuu <koj1555@naver.com>
2025-07-05 10:21:01 +0900

    Feat: img

--- reddiee-front/src/components/Home/HomePage.tsx
+++ reddiee-front/src/components/Home/HomePage.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+// 메인페이지
 export default function HomePage() {
   return (
-    <div className="text-primary">
-      {/* 메인 */}
-      <div>홈페이지</div>
+    <div className="flex items-start flex-col">
+      <img alt="wave image" src="/wave.png" />
+      <img alt="wave image" src="/wave.png" />
     </div>
   );
 }
