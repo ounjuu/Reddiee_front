2b0bdc68b54ddd43b918bbb60d9fde9603971e4b
ounjuu <koj1555@naver.com>
2025-09-11 09:07:27 +0900

    fix: key

--- reddiee-front/src/components/ProductGrid/ProductGrid.tsx
+++ reddiee-front/src/components/ProductGrid/ProductGrid.tsx
@@ -38,11 +38,8 @@ export default function ProductGrid({ products }: { products: Product[] }) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-6 ">
       {products.map((product) => (
-        <div>
-          <div
-            key={product.id}
-            className="border rounded-lg overflow-hidden shadow-sm hover:shadow-md transition"
-          >
+        <div key={product.id}>
+          <div className="border rounded-lg overflow-hidden shadow-sm hover:shadow-md transition">
             <img
               src={`${process.env.NEXT_PUBLIC_API_URL}${product.imageUrl}`}
               alt={product.name}
