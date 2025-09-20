eaa7b18a067c47fc015c17085bf531a025826cfe
ounjuu <koj1555@naver.com>
2025-09-20 15:01:55 +0900

    fix: 색

--- reddiee-front/src/components/ProductGrid/ProductGrid.tsx
+++ reddiee-front/src/components/ProductGrid/ProductGrid.tsx
@@ -144,7 +144,7 @@ export default function ProductGrid({ products }: { products: Product[] }) {
                     취소
                   </button>
                   <button
-                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    className="px-4 py-2 bg-reddieetext text-white rounded hover:bg-blue-600"
                     onClick={() => handleConfirm(product.id)}
                   >
                     확인
